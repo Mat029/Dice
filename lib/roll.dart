9f21ce0d6b51ec4c413ce8f50b7129d2d712a568
Mat029 <boulabgerp@gmail.com>
2021-06-26 19:28:06 +0200

    Dev 1.1 : 10 variables randoms

--- lib/roll.dart
+++ lib/roll.dart
@@ -16,17 +16,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int randomNumber = 1;
+  int randomNumber1 = 1;
+  int randomNumber2 = 1;
+  int randomNumber3 = 1;
+  int randomNumber4 = 1;
+  int randomNumber5 = 1;
+  int randomNumber6 = 1;
+  int randomNumber7 = 1;
+  int randomNumber8 = 1;
+  int randomNumber9 = 1;
+  int randomNumber10 = 1;
+
   Random random = new Random();
 
   void initState() {
     super.initState();
-    randomNumber = 1 + random.nextInt(widget.nboffaces);
+    randomNumber1 = 1 + random.nextInt(widget.nboffaces);
+    randomNumber2 = 1 + random.nextInt(widget.nboffaces);
+    randomNumber3 = 1 + random.nextInt(widget.nboffaces);
+    randomNumber4 = 1 + random.nextInt(widget.nboffaces);
+    randomNumber5 = 1 + random.nextInt(widget.nboffaces);
+    randomNumber6 = 1 + random.nextInt(widget.nboffaces);
+    randomNumber7 = 1 + random.nextInt(widget.nboffaces);
+    randomNumber8 = 1 + random.nextInt(widget.nboffaces);
+    randomNumber9 = 1 + random.nextInt(widget.nboffaces);
+    randomNumber10 = 1 + random.nextInt(widget.nboffaces);
   }
 
   void _incrementCounter() {
     setState(() {
-      randomNumber = 1 + random.nextInt(widget.nboffaces);
+      randomNumber1 = 1 + random.nextInt(widget.nboffaces);
+      randomNumber2 = 1 + random.nextInt(widget.nboffaces);
+      randomNumber3 = 1 + random.nextInt(widget.nboffaces);
+      randomNumber4 = 1 + random.nextInt(widget.nboffaces);
+      randomNumber5 = 1 + random.nextInt(widget.nboffaces);
+      randomNumber6 = 1 + random.nextInt(widget.nboffaces);
+      randomNumber7 = 1 + random.nextInt(widget.nboffaces);
+      randomNumber8 = 1 + random.nextInt(widget.nboffaces);
+      randomNumber9 = 1 + random.nextInt(widget.nboffaces);
+      randomNumber10 = 1 + random.nextInt(widget.nboffaces);
     });
   }
 
@@ -49,7 +77,16 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 80,
             ),
             ImageDice(
-              random: randomNumber,
+              random1: randomNumber1,
+              random2: randomNumber2,
+              random3: randomNumber3,
+              random4: randomNumber4,
+              random5: randomNumber5,
+              random6: randomNumber6,
+              random7: randomNumber7,
+              random8: randomNumber8,
+              random9: randomNumber9,
+              random10: randomNumber10,
               face: widget.nboffaces,
               dice: widget.nbofdice,
             ),
@@ -79,10 +116,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class ImageDice extends StatefulWidget {
   ImageDice(
-      {Key? key, required this.face, required this.random, required this.dice})
+      {Key? key,
+      required this.face,
+      required this.random1,
+      required this.random2,
+      required this.random3,
+      required this.random4,
+      required this.random5,
+      required this.random6,
+      required this.random7,
+      required this.random8,
+      required this.random9,
+      required this.random10,
+      required this.dice})
       : super(key: key);
   final int face;
-  final int random;
+  final int random1;
+  final int random2;
+  final int random3;
+  final int random4;
+  final int random5;
+  final int random6;
+  final int random7;
+  final int random8;
+  final int random9;
+  final int random10;
   final int dice;
 
   @override
@@ -100,7 +158,16 @@ class _ImageDiceState extends State<ImageDice> {
   ];
   @override
   Widget build(BuildContext context) {
-    int a = widget.random;
+    int r1 = widget.random1;
+    int r2 = widget.random2;
+    int r3 = widget.random3;
+    int r4 = widget.random4;
+    int r5 = widget.random5;
+    int r6 = widget.random6;
+    int r7 = widget.random7;
+    int r8 = widget.random8;
+    int r9 = widget.random9;
+    int r10 = widget.random10;
     double c = 0;
     double x = 1;
     if (widget.dice == 2) {
@@ -115,14 +182,14 @@ class _ImageDiceState extends State<ImageDice> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Image.asset(
-            "image/" + imageArray[widget.random - 1],
+            "image/" + imageArray[r1 - 1],
             height: (175 * x) + 25,
             width: (175 * x) + 25,
           ),
         ],
       );
     } else {
-      if (a == 100) {
+      if (r1 == 100) {
         c = 100;
       }
       return Row(
@@ -141,7 +208,7 @@ class _ImageDiceState extends State<ImageDice> {
                 child: Align(
                   alignment: Alignment.center,
                   child: Text(
-                    "$a",
+                    "$r1",
                     textScaleFactor: (10.8 * x) + 1.2,
                     style: TextStyle(
                       color: Colors.black,
